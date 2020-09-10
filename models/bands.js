6b35d71cfbac078204112dd6594c07669f3ed209
BalFaz <balfaz@gmail.com>
2020-09-10 10:51:25 +0200

    Server funzionante in locale

---
+++ models/bands.js
@@ -0,0 +1,31 @@
+
+class Bands {
+    constructor() {
+        this.bands = [];
+    }
+
+    addBand(band = new Bands) {
+        this.bands.push(band);
+    }
+
+    getBands() {
+        return this.bands;
+    }
+
+    deleteBand(id = '') {
+        this.bands = this.bands.filter(band => band.id != id);
+    }
+
+    voteBand(id = '') {
+        this.bands = this.bands.map(band => {
+            if (band.id === id) {
+                band.votes++;
+                return band;
+            } else {
+                return band;
+            }
+        })
+    }
+}
+
+module.exports = Bands
--- sockets/socket.js
+++ sockets/socket.js
@@ -1,7 +1,19 @@
 const { io } = require('../index');
+const Bands = require('../models/bands');
+const Band = require('../models/band');
+
+const bands = new Bands();
+
+bands.addBand(new Band('Pantera'));
+bands.addBand(new Band('Apocalyptica'));
+bands.addBand(new Band('Queen'));
+bands.addBand(new Band('Pink Floyd'));
 
 //socket Message
 io.on('connect', client => {
+
+    client.emit('active-bands', bands.getBands());
+
     console.log('client connect');
     client.on('disconnect', () => {
         console.log('client disconnect');
@@ -10,6 +22,27 @@ io.on('connect', client => {
     client.on('message', (payload) => {
         console.log('messaggio', payload);
         io.emit('message', { admin: 'New Message' });
+    });
+
+    client.on('new-message', (payload) => {
+        //io.emit('new-message', payload);
+        client.broadcast.emit('new-message', payload);
+    });
+
+    client.on('vote-band', (payload) => {
+        bands.voteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+    });
+
+    client.on('add-band', (payload) => {
+        const newBand = new Band(payload.name);
+        bands.addBand(newBand);
+        io.emit('active-bands', bands.getBands());
+    });
+
+    client.on('delete-band', (payload) => {
+        bands.deleteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
     });
 });
 
